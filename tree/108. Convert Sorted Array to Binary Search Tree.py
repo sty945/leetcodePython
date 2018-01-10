597901ad27fbab53e9737583b4aaf4faa45fbc2b
sty945 <sutaoyu@yeah.net>
2018-01-10 20:07:31 +0800

    将一个顺序递增得列表转换成一个平衡二叉树

---
+++ tree/108. Convert Sorted Array to Binary Search Tree.py
@@ -0,0 +1,72 @@
+# Definition for a binary tree node.
+import json
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def sortedArrayToBST(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        input: [-10,-3,0,5,9]
+        output: [0, -3, 9, -10, null, 5, null, null, null, null, null]
+        """
+        if not nums:
+            return None
+        mid = len(nums) // 2
+        root = TreeNode(nums[mid])
+        root.left = self.sortedArrayToBST(nums[:mid])
+        root.right = self.sortedArrayToBST(nums[mid + 1:])
+        return root
+
+
+def stringToIntegerList(input):
+    return json.loads(input)
+
+
+def treeNodeToString(root):
+    if not root:
+        return "[]"
+    output = ""
+    queue = [root]
+    current = 0
+    while current != len(queue):
+        node = queue[current]
+        current = current + 1
+
+        if not node:
+            output += "null, "
+            continue
+
+        output += str(node.val) + ", "
+        queue.append(node.left)
+        queue.append(node.right)
+    return "[" + output[:-2] + "]"
+
+
+def main():
+    import sys
+    def readlines():
+        for line in sys.stdin:
+            yield line.strip('\n')
+
+    lines = readlines()
+    while True:
+        try:
+            line = lines.__next__()
+            nums = stringToIntegerList(line);
+
+            ret = Solution().sortedArrayToBST(nums)
+
+            out = treeNodeToString(ret);
+            print(out)
+        except StopIteration:
+            break
+
+
+if __name__ == '__main__':
+    main()
