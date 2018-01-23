6d1a7a4ffcce0fea774ec7d887e13bfc3a21998c
sty945 <sutaoyu@yeah.net>
2018-01-23 20:23:34 +0800

    非递归中序遍历

---
+++ tree/94. Binary Tree Inorder Traversal.py
@@ -0,0 +1,33 @@
+# !/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+
+# Time: 2018/1/23 20:11
+
+# Author: sty
+
+# File: 94. Binary Tree Inorder Traversal.py
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res, stack = [], []
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+            if not stack:
+                return res
+            node = stack.pop()
+            res.append(node.val)
+            root = node.right
