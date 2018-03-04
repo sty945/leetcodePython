1c64c26de3766f371156b3ecfcb4bdf1f31a4d9d
sty945 <sutaoyu@yeah.net>
2018-03-04 21:37:31 +0800

    判断一个字符串是，只看字母和数字是不是回文串

---
+++ Array/125. Valid Palindrome.py
@@ -0,0 +1,32 @@
+# !/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+
+# Time: 2018/2/20 11:40
+
+# Author: sty
+
+# File: 125. Valid Palindrome.py
+
+class Solution:
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        if not s:
+            return True
+        s = s.lower()
+        res = ''
+        for i in range(len(s)):
+            if 'a' <= s[i] <= 'z' or '0' <= s[i] <= '9':
+                res += s[i]
+        low, high = 0, len(res) - 1
+        while low < high:
+            if res[low] == res[high]:
+                low += 1
+                high -= 1
+            else:
+                return False
+        return True
+
