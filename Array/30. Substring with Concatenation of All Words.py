3198a2ca37eb5037756dfe4dc127485b90148ac0
sty945 <sutaoyu@yeah.net>
2018-05-12 22:02:58 +0800

    30. 与所有单词相关联的字串 给定一个字符串 s 和一些长度相同的单词 words。在 s 中找出可以恰好串联 words 中所有单词的子串的起始位置。

--- Array/30. Substring with Concatenation of All Words.py
+++ Array/30. Substring with Concatenation of All Words.py
@@ -7,3 +7,42 @@
 # Author: sty
 
 # File: 30. Substring with Concatenation of All Words.py
+
+from collections import defaultdict
+
+class Solution:
+    def findSubstring(self, s, words):
+        """
+        :type s: str
+        :type words: List[str]
+        :rtype: List[int]
+        """
+        res = []
+        if len(words) == 0 or len(s) < len(words[0]) * len(words):
+            return res
+        str_len = len(s)
+        word_len = len(words[0])
+        total_len = len(words) * word_len
+        map_dict = defaultdict(int)
+        cur_dict = defaultdict(int)
+        for word in words:
+            map_dict[word] += 1
+
+        for start in range(0, str_len - total_len + 1):
+            end = start
+            while end < start + total_len:
+                substr = s[end: end + word_len]
+                cur_dict[substr] += 1
+                # 如果当前dict中还有之前的没有的，或者值比至之前的大都终止
+                if cur_dict[substr] > map_dict[substr]:
+                    break
+                end += word_len
+            if end == start + total_len:
+                res.append(start)
+            cur_dict.clear()
+        return res
+
+
+
+
+
