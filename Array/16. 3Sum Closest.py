22397127cb075db90fdf3cbbb20228e7d949919a
sty945 <sutaoyu@yeah.net>
2018-08-02 09:04:15 +0800

    3sum closest

---
+++ Array/16. 3Sum Closest.py
@@ -0,0 +1,18 @@
+# coding:utf-8
+
+# !/usr/bin/env python
+
+# Time: 2018/8/2 9:03
+
+# Author: sty
+
+# File: 16. 3Sum Closest.py
+
+
+class Solution:
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
