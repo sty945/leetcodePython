299e0d7cfb30acb9bf8cf3e7f815e1e5d3e03bba
sty945 <sutaoyu@yeah.net>
2018-08-07 09:40:31 +0800

    最近的重复值

--- Array/219. Contains Duplicate II.py
+++ Array/219. Contains Duplicate II.py
@@ -8,3 +8,20 @@
 
 # File: 219. Contains Duplicate II.py
 
+
+class Solution:
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        length = len(nums)
+        if length < 2:
+            return False
+        res_num = {}
+        for i in range(length):
+            if nums[i] in res_num and i - res_num[nums[i]] <= k:
+                return True
+            res_num[nums[i]] = i
+        return False
