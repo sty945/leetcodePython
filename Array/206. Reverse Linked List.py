d7735834ebb75ca2627bc3318c13692686c59828
sty945 <sutaoyu@yeah.net>
2018-08-15 00:04:42 +0800

    翻转列表

---
+++ Array/206. Reverse Linked List.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# !/usr/bin/env python
+
+# Time: 2018/8/14 23:19
+
+# Author: sty
+
+# File: 206. Reverse Linked List.py
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
