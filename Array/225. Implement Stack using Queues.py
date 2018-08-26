4ac6c453eae2a1a5e28b233aeeea4e8b2fd58ce1
sty945 <sutaoyu@yeah.net>
2018-08-26 17:48:25 +0800

    通过队列来实现栈

---
+++ Array/225. Implement Stack using Queues.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+# !/usr/bin/env python
+
+# Time: 2018/8/26 17:18
+
+# Author: sty
+
+# File: 225. Implement Stack using Queues.py
+
+import collections
+class MyStack:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.queue = collections.deque()
+
+    def push(self, x):
+        """
+        Push element x onto stack.
+        :type x: int
+        :rtype: void
+        """
+        self.queue.append(x)
+
+    def pop(self):
+        """
+        Removes the element on top of the stack and returns that element.
+        :rtype: int
+        """
+        for i in range(len(self.queue) - 1):
+            self.queue.append(self.queue.popleft())
+        return self.queue.popleft()
+
+    def top(self):
+        """
+        Get the top element.
+        :rtype: int
+        """
+        return self.queue[-1]
+
+    def empty(self):
+        """
+        Returns whether the stack is empty.
+        :rtype: bool
+        """
+        if len(self.queue) == 0:
+            return True
+        else:
+            return False
