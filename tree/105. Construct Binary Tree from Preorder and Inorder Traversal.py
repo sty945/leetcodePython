d497f1f55af25396d8912bc5ce4fbd190b8f983d
sty945 <sutaoyu@yeah.net>
2018-08-26 15:13:44 +0800

    有前序和中序来重建二叉树

--- tree/105. Construct Binary Tree from Preorder and Inorder Traversal.py
+++ tree/105. Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -16,6 +16,14 @@ class TreeNode:
         self.right = None
 
 class Solution:
+    def buildTree0(self, preorder, inorder):
+        if inorder:
+            ind = inorder.index(preorder.pop(0))
+            root = TreeNode(inorder[ind])
+            root.left = self.buildTree(preorder, inorder[0:ind])
+            root.right = self.buildTree(preorder, inorder[ind + 1:])
+            return root
+
     def buildTree(self, preorder, inorder):
         """
         :type preorder: List[int]
